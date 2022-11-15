b1dbf3ed32df327a59291a0e575b11e144dd2323
82258624@kt.com <82258624@kt.com>
2022-11-15 18:10:13 +0900

    init

---
+++ cicd_test_cjs1/src/main/java/icis/kt/co/kr/cicd_test_cjs1/App.java
@@ -0,0 +1,13 @@
+package icis.kt.co.kr.cicd_test_cjs1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
